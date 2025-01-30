765e63e8b5186cc7f8638cb73ed54ea97c846678
marwen <marwen.mrabti.me@gmail.com>
2025-01-31 00:31:48 +0100

    update metadata

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,9 @@
 import { LoginForm } from "@/components/login-form";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
 
 export default function SignInPage() {
   return (
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+
 export default function SignUpPage() {
   return <div>create something cool</div>;
 }
